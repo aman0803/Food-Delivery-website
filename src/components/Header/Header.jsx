4c3d53bb26cf26b0f518a0590c051c19169c0341
amanBeast <94213164+aman0803@users.noreply.github.com>
2024-06-24 10:43:43 +0530

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order your favourite food here</h2>
+            <p>Choose from a diverse menu featuring a delactable array of dishes crafted with the finest ingredients and culinary expertise.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { useContext, useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom';
+import { StoreContext } from '../../Context/StoreContext';
+
+
+const Navbar = ({setShowLogin}) => {
+
+    const [menu , setMenu] = useState("Home");
+
+    const {getTotalCartAmount} = useContext(StoreContext);
+  return (
+    <div className='navbar'>
+        <Link to= '/'><img src={assets.logo} alt="" className="logo" /></Link>
+        <ul className="navbar-menu">
+            <Link to = '/' onClick={() => setMenu("Home")} className={menu === "Home" ? "active" : ""}>Home</Link>
+            <a href = '#explore-menu' onClick={() => setMenu("Menu")} className={menu === "Menu" ? "active" : ""}>Menu</a>
+            <a href = '#app-download' onClick={() => setMenu("Mobile-app")} className={menu === "Mobile-app" ? "active" : ""}>Mobile-app</a>
+            <a href = '#footer' onClick={() => setMenu("Contact Us")} className={menu === "Contact Us" ? "active" : ""}>Contact Us</a>
+        </ul>
+        <div className="navbar-right">
+            <img src={assets.search_icon} alt='' />
+            <div className="navbar-search-icon">
+                <Link to= '/cart'><img src={assets.basket_icon} alt='' /></Link>
+                <div className={getTotalCartAmount() ===0 ? "" : "dot"}></div>
+            </div>
+            <button onClick={() => setShowLogin(true)}>Sign In</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
